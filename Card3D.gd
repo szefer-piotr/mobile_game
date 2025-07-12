cdd99bd62d737402cfac9bd60a08ebf87dc3170b
Piotr Szefer <szefer85@gmail.com>
2025-07-12 09:02:56 +0200

    Card draw bug NOT fixed yet.

--- Card3D.gd
+++ Card3D.gd
@@ -1,9 +1,21 @@
 extends Node3D
 
 @export var value: int = 0
-@export var target_position: Vector3 = Vector3()
+@export var target_position: Vector3
 
 var label_assigned := false
+
+func set_target_position(pos: Vector3):
+	target_position = pos
+	if is_inside_tree():
+		animate_to_position()
+	else:
+		await ready
+		animate_to_position()
+
+func animate_to_position():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "global_position", target_position, 0.4).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
 
 func _ready():
 	set_process(true)
