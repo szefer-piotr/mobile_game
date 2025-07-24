cb8d70e92b3df6cef221e84d5ac6e12c6f0502c5
Piotr Szefer <szefer85@gmail.com>
2025-07-24 20:48:31 +0200

    NPC walks around but animation does not repeat.

--- Knight.gd
+++ Knight.gd
@@ -2,18 +2,27 @@ extends CharacterBody3D
 
 @export var speed: float = 2.0
 @export var area_size: float = 8.0
+@export var idle_time_range: Vector2 = Vector2(1.5, 3.0)
+@export var move_time_range: Vector2 = Vector2(3.0, 7.0)
 
 @onready var anim_player: AnimationPlayer = $Knight/AnimationPlayer
+@onready var knight_model: Node3D = $Knight
+@onready var timer: Timer = $Timer
 
 var target_pos: Vector3
+var is_idle := false
 
 func _ready():
+	knight_model.rotation_degrees.y = 180
 	randomize()
-	pick_new_target()
-	if anim_player:
-		anim_player.play("Walking_A")
+	timer.one_shot = true
+	timer.connect("timeout", _on_Timer_timeout)
+	enter_move_state()
 		
 func _physics_process(delta: float) -> void:
+	if is_idle:
+		return
+		
 	var to_target = target_pos - global_position
 	to_target.y = 0
 	if to_target.length() < 0.2:
@@ -32,3 +41,23 @@ func pick_new_target():
 	var x = randf_range(-area_size, area_size)
 	var z = randf_range(-area_size, area_size)
 	target_pos = Vector3(x, global_position.y, z)
+
+func enter_move_state():
+	is_idle = false
+	pick_new_target()
+	if anim_player:
+		anim_player.play("Walking_A")
+	timer.start(randf_range(move_time_range.x, move_time_range.y))
+	
+func enter_idle_state():
+	is_idle = true
+	velocity = Vector3.ZERO
+	if anim_player:
+		anim_player.play("Idle")
+	timer.start(randf_range(idle_time_range.x, idle_time_range.y))
+	
+func _on_Timer_timeout():
+	if is_idle:
+		enter_move_state()
+	else:
+		enter_idle_state()
