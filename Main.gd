538adc15e9ed245280719cb345f0a937c9e7088b
Piotr Szefer <szefer85@gmail.com>
2025-08-05 18:44:37 +0200

    Tab fix.

--- Main.gd
+++ Main.gd
@@ -17,81 +17,81 @@ var card_table_scene = preload("res://card_table.tscn")
 var card_table_root: Node3D = null
 
 func hide_building_ui():
-				$CanvasLayer/KingdomPanel.visible = false
-				$CanvasLayer/KingdomButton.visible = true
-				$CanvasLayer/CardTableButton.visible = true
+	$CanvasLayer/KingdomPanel.visible = false
+	$CanvasLayer/KingdomButton.visible = true
+	$CanvasLayer/CardTableButton.visible = true
 
 func hide_kingdom_mode():
-		hide_building_ui()
-		var tween = get_tree().create_tween()
-		tween.tween_property(cam, "global_position", default_cam_pos, 0.05)
-		tween.tween_property(cam, "rotation_degrees", default_cam_rot, 0.25)
+	hide_building_ui()
+	var tween = get_tree().create_tween()
+	tween.tween_property(cam, "global_position", default_cam_pos, 0.05)
+	tween.tween_property(cam, "rotation_degrees", default_cam_rot, 0.25)
 
 func _unhandled_input(event):
-		if $CanvasLayer/KingdomPanel.visible and event is InputEventMouseButton:
-				if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-						var rect = $CanvasLayer/KingdomPanel.get_global_rect()
-						if not rect.has_point(event.position):
-								hide_kingdom_mode()
+	if $CanvasLayer/KingdomPanel.visible and event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var rect = $CanvasLayer/KingdomPanel.get_global_rect()
+			if not rect.has_point(event.position):
+				hide_kingdom_mode()
 
 func setup_kingdoms():
-		kingdoms.clear()
-		var k1 = KingdomData.new()
-		k1.name = "Kingdom 1"
-		k1.scene_path = "res://kingdoms/Kingdom01.tscn"
-		k1.buildings = KingdomData.kingdom1_buildings()
-		kingdoms.append(k1)
+	kingdoms.clear()
+	var k1 = KingdomData.new()
+	k1.name = "Kingdom 1"
+	k1.scene_path = "res://kingdoms/Kingdom01.tscn"
+	k1.buildings = KingdomData.kingdom1_buildings()
+	kingdoms.append(k1)
 
-		var k2 = KingdomData.new()
-		k2.name = "Kingdom 2"
-		k2.scene_path = "res://kingdoms/Kingdom02.tscn"
-		k2.buildings = KingdomData.kingdom2_buildings()
-		kingdoms.append(k2)
+	var k2 = KingdomData.new()
+	k2.name = "Kingdom 2"
+	k2.scene_path = "res://kingdoms/Kingdom02.tscn"
+	k2.buildings = KingdomData.kingdom2_buildings()
+	kingdoms.append(k2)
 
-		var k3 = KingdomData.new()
-		k3.name = "Kingdom 3"
-		k3.scene_path = "res://kingdoms/Kingdom03.tscn"
-		k3.buildings = KingdomData.kingdom3_buildings()
-		kingdoms.append(k3)
+	var k3 = KingdomData.new()
+	k3.name = "Kingdom 3"
+	k3.scene_path = "res://kingdoms/Kingdom03.tscn"
+	k3.buildings = KingdomData.kingdom3_buildings()
+	kingdoms.append(k3)
 
 func load_kingdom(index: int):
-		if index >= kingdoms.size():
-				return
-		current_kingdom_idx = index
-		var data: KingdomData = kingdoms[index]
-		buildings.clear()
-		for key in data.buildings.keys():
-				var entry = data.buildings[key]
-				buildings[key] = {
-						"level": entry.get("level", 0),
-						"costs": entry.get("costs", []).duplicate()
-				}
-		var old_root = get_node_or_null("KingdomRoot")
-		if old_root:
-				old_root.queue_free()
-		var new_root: Node3D = null
-		if data.scene_path != "":
-				var scene = load(data.scene_path)
-				print(data.scene_path)
-				if scene:
-						new_root = scene.instantiate()
-		if new_root == null:
-				new_root = Node3D.new()
-		new_root.name = "KingdomRoot"
-		add_child(new_root)
-		for key in buildings.keys():
-				var label = get_building_label(key)
-				if label:
-						label.text = "%s (Lv. %d)" % [key, buildings[key]["level"]]
-		var buildings_node = new_root.get_node_or_null("Buildings")
-		if buildings_node:
-				for building in buildings_node.get_children():
-						building.visible = false
-						var key_name = building.name.replace("_", " ")
-						if buildings.has(key_name) and buildings[key_name]["level"] > 0:
-								building.visible = true
-		connect_building_buttons()
-		update_all_building_buttons()
+	if index >= kingdoms.size():
+		return
+	current_kingdom_idx = index
+	var data: KingdomData = kingdoms[index]
+	buildings.clear()
+	for key in data.buildings.keys():
+		var entry = data.buildings[key]
+		buildings[key] = {
+			"level": entry.get("level", 0),
+			"costs": entry.get("costs", []).duplicate()
+		}
+	var old_root = get_node_or_null("KingdomRoot")
+	if old_root:
+		old_root.queue_free()
+	var new_root: Node3D = null
+	if data.scene_path != "":
+		var scene = load(data.scene_path)
+		print(data.scene_path)
+		if scene:
+			new_root = scene.instantiate()
+	if new_root == null:
+		new_root = Node3D.new()
+	new_root.name = "KingdomRoot"
+	add_child(new_root)
+	for key in buildings.keys():
+		var label = get_building_label(key)
+		if label:
+			label.text = "%s (Lv. %d)" % [key, buildings[key]["level"]]
+	var buildings_node = new_root.get_node_or_null("Buildings")
+	if buildings_node:
+		for building in buildings_node.get_children():
+			building.visible = false
+			var key_name = building.name.replace("_", " ")
+			if buildings.has(key_name) and buildings[key_name]["level"] > 0:
+				building.visible = true
+	connect_building_buttons()
+	update_all_building_buttons()
 
 func check_kingdom_complete():
 	for key in buildings.keys():
@@ -102,56 +102,57 @@ func check_kingdom_complete():
 	return true
 
 func _ready():
-                randomize()
-                default_cam_pos = cam.global_position
-                default_cam_rot = cam.rotation_degrees
-                setup_kingdoms()
-                print("Loading kingdoms...")
-                load_kingdom(0)
-                cam.global_position = default_cam_pos
-                cam.rotation_degrees = default_cam_rot
-                show_building_ui()
+	randomize()
+	default_cam_pos = cam.global_position
+	default_cam_rot = cam.rotation_degrees
+	setup_kingdoms()
+	print("Loading kingdoms...")
+	load_kingdom(0)
+	cam.global_position = default_cam_pos
+	cam.rotation_degrees = default_cam_rot
+	show_building_ui()
+
 func _on_KingdomButton_pressed():
-                show_kingdom_mode()
+	show_kingdom_mode()
 
 func show_card_table():
-		hide_building_ui()
-		$CanvasLayer/KingdomButton.visible = false
-		$CanvasLayer/CardTableButton.visible = false
-                var kroot = get_node_or_null("KingdomRoot")
-                if kroot:
-                                kroot.visible = false
-                if card_table_root == null:
-                                card_table_root = card_table_scene.instantiate()
-                                add_child(card_table_root)
-                card_table_root.show()
-                var ct_cam = card_table_root.get_node_or_null("Camera3D")
-                if ct_cam:
-                                ct_cam.make_current()
+	hide_building_ui()
+	$CanvasLayer/KingdomButton.visible = false
+	$CanvasLayer/CardTableButton.visible = false
+	var kroot = get_node_or_null("KingdomRoot")
+	if kroot:
+		kroot.visible = false
+	if card_table_root == null:
+		card_table_root = card_table_scene.instantiate()
+		add_child(card_table_root)
+	card_table_root.show()
+	var ct_cam = card_table_root.get_node_or_null("Camera3D")
+	if ct_cam:
+		ct_cam.make_current()
 
 func hide_card_table():
-                if card_table_root:
-                                card_table_root.hide()
-		cam.make_current()
-		cam.global_position = default_cam_pos
-		cam.rotation_degrees = default_cam_rot
-		var kroot = get_node_or_null("KingdomRoot")
-		if kroot:
-				kroot.visible = true
-		hide_building_ui()
+	if card_table_root:
+		card_table_root.hide()
+	cam.make_current()
+	cam.global_position = default_cam_pos
+	cam.rotation_degrees = default_cam_rot
+	var kroot = get_node_or_null("KingdomRoot")
+	if kroot:
+		kroot.visible = true
+	hide_building_ui()
 
 func show_kingdom_mode():
-                var tween = get_tree().create_tween()
-                tween.tween_property(cam, "global_position", Vector3(0, 10, 10), 0.5)
-                tween.tween_property(cam, "rotation_degrees", Vector3(-40, 0, 0), 0.15)
-                if card_table_root:
-                                card_table_root.hide()
-                show_building_ui()
+	var tween = get_tree().create_tween()
+	tween.tween_property(cam, "global_position", Vector3(0, 10, 10), 0.5)
+	tween.tween_property(cam, "rotation_degrees", Vector3(-40, 0, 0), 0.15)
+	if card_table_root:
+		card_table_root.hide()
+	show_building_ui()
 
 func show_building_ui():
-                $CanvasLayer/KingdomPanel.visible = true
-                $CanvasLayer/KingdomButton.visible = false
-                $CanvasLayer/CardTableButton.visible = false
+	$CanvasLayer/KingdomPanel.visible = true
+	$CanvasLayer/KingdomButton.visible = false
+	$CanvasLayer/CardTableButton.visible = false
 	var kroot = get_node_or_null("KingdomRoot")
 	if kroot:
 		kroot.visible = true
@@ -175,65 +176,65 @@ func show_building_ui():
 	update_all_building_buttons()
 
 func get_building_button(key: String) -> Button:
-		var base = key.replace(" ", "")
-		return $CanvasLayer/KingdomPanel/ScrollContainer/BuildingList.get_node_or_null("%s/Button" % base)
+	var base = key.replace(" ", "")
+	return $CanvasLayer/KingdomPanel/ScrollContainer/BuildingList.get_node_or_null("%s/Button" % base)
 
 func get_building_label(key: String) -> Label:
-		var base = key.replace(" ", "")
-		return $CanvasLayer/KingdomPanel/ScrollContainer/BuildingList.get_node_or_null("%s/Label" % base)
+	var base = key.replace(" ", "")
+	return $CanvasLayer/KingdomPanel/ScrollContainer/BuildingList.get_node_or_null("%s/Label" % base)
 
 func connect_building_buttons():
-		for key in buildings.keys():
-				var btn = get_building_button(key)
-				if btn:
-						if btn.pressed.get_connections():
-								for c in btn.pressed.get_connections():
-										btn.pressed.disconnect(c.callable)
-						btn.pressed.connect(_on_BuildingButton_pressed.bind(key))
+	for key in buildings.keys():
+		var btn = get_building_button(key)
+		if btn:
+			if btn.pressed.get_connections():
+				for c in btn.pressed.get_connections():
+					btn.pressed.disconnect(c.callable)
+			btn.pressed.connect(_on_BuildingButton_pressed.bind(key))
 
 func _on_BuildingButton_pressed(key: String):
-		if not buildings.has(key):
-				return
-		var data = buildings[key]
-		var lvl = data["level"]
-		if lvl < data["costs"].size():
-				var cost = data["costs"][lvl]
-				if CurrencyManager.spend_coins(cost):
-						data["level"] += 1
-						buildings[key] = data
-						var label = get_building_label(key)
-						if label:
-								label.text = "%s (Lv. %d)" % [key, data["level"]]
-						var node_name = key.replace(" ", "")
-						var buildings_node = $KingdomRoot.get_node_or_null("Buildings")
-						if buildings_node:
-								var build_node = buildings_node.get_node_or_null(node_name)
-								if build_node:
-										build_node.visible = true
-										if key == "Castle" and data["level"] == 1:
-												var npc = knight_scene.instantiate()
-												npc.global_position = build_node.global_position + Vector3(2, 0, 0)
-												npc.follow_center_node = build_node
-												npc.area_size = 5
-												$KingdomRoot.add_child(npc)
-				update_building_button(key, data)
-				check_kingdom_complete()
+	if not buildings.has(key):
+		return
+	var data = buildings[key]
+	var lvl = data["level"]
+	if lvl < data["costs"].size():
+		var cost = data["costs"][lvl]
+		if CurrencyManager.spend_coins(cost):
+			data["level"] += 1
+			buildings[key] = data
+			var label = get_building_label(key)
+			if label:
+				label.text = "%s (Lv. %d)" % [key, data["level"]]
+			var node_name = key.replace(" ", "")
+			var buildings_node = $KingdomRoot.get_node_or_null("Buildings")
+			if buildings_node:
+				var build_node = buildings_node.get_node_or_null(node_name)
+				if build_node:
+					build_node.visible = true
+					if key == "Castle" and data["level"] == 1:
+						var npc = knight_scene.instantiate()
+						npc.global_position = build_node.global_position + Vector3(2, 0, 0)
+						npc.follow_center_node = build_node
+						npc.area_size = 5
+						$KingdomRoot.add_child(npc)
+	update_building_button(key, data)
+	check_kingdom_complete()
 
 func update_building_button(key: String, data: Dictionary):
-		var btn = get_building_button(key)
-		if btn == null:
-				return
-		var lvl = data["level"]
-		if lvl < data["costs"].size():
-				var cost = data["costs"][lvl]
-				if lvl == 0:
-						btn.text = "Build (%d)" % cost
-				else:
-						btn.text = "Upgrade (%d)" % cost
+	var btn = get_building_button(key)
+	if btn == null:
+		return
+	var lvl = data["level"]
+	if lvl < data["costs"].size():
+		var cost = data["costs"][lvl]
+		if lvl == 0:
+			btn.text = "Build (%d)" % cost
 		else:
-				btn.text = "MAXED"
-				btn.disabled = true
+			btn.text = "Upgrade (%d)" % cost
+	else:
+		btn.text = "MAXED"
+		btn.disabled = true
 
 func update_all_building_buttons():
-		for key in buildings.keys():
-				update_building_button(key, buildings[key])
+	for key in buildings.keys():
+		update_building_button(key, buildings[key])
